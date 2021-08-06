82136bd7ed79ed4948f6132878e0681651370ed7
esam771 <75175872+esam771@users.noreply.github.com>
2021-08-06 13:07:28 -0500

    Update CodingProjectJUnit.java

--- CodingProjectJUnit.java
+++ CodingProjectJUnit.java
@@ -10,6 +10,9 @@ class CodingProjectJUnit {
 	void test() {
 		LargestPrimeFactor s = new LargestPrimeFactor();
 		s.FindPrimeFactor(13195); //prints every prime factor
+		
+		CollatzConjectureTest c = new CollatzConjectureTest();
+		c.CollatzConjecture(23954533); //prints collatz conjecture process
 	}
 
 }
