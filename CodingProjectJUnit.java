05894c412fc8145950c6071da2f2b0e82adfa42d
esam771 <75175872+esam771@users.noreply.github.com>
2021-08-08 18:29:28 -0500

    Update CodingProjectJUnit.java

--- CodingProjectJUnit.java
+++ CodingProjectJUnit.java
@@ -8,11 +8,56 @@ class CodingProjectJUnit {
 
 	@Test
 	void test() {
-		LargestPrimeFactor s = new LargestPrimeFactor();
-		s.FindPrimeFactor(13195); //prints every prime factor
 		
-		CollatzConjectureTest c = new CollatzConjectureTest();
-		c.CollatzConjecture(23954533); //prints collatz conjecture process
-	}
+		//switch for choosing class to test
+		 int testnum = 3;
+		 
+	        switch (testnum) {
+	            case 1:  
+	        	//https:projecteuler.net/problem=3
+	        	LargestPrimeFactor a = new LargestPrimeFactor();
+	        	a.FindPrimeFactor(5643); //prints every prime factor
+	                     break;
+	            case 2:  
+	            	//inspired by youtube video
+	            	CollatzConjectureTest b = new CollatzConjectureTest();
+	        	b.CollatzConjecture(3456); //prints collatz conjecture process
+	                     break;
+	            case 3:  
+	            	//https://projecteuler.net/problem=50
+	        	ConsecutivePrimeSum c = new ConsecutivePrimeSum();
+	        	c.ConsecutivePrime(2345); //finds
+	                     break;
+	            case 4:  
+	            	
+	                     break;
+	            case 5:  
 
+	                     break;
+	            case 6:  
+
+	                     break;
+	            case 7:  
+
+	                     break;
+	            case 8:  
+
+	                     break;
+	            case 9:  
+
+	                     break;
+	            case 10: 
+
+	                     break;
+	            case 11: 
+
+	                     break;
+	            case 12: 
+
+	                     break;
+	                     
+	            default: System.out.println("No case to test for input");
+	                     break;
+	        }
+	}
 }
